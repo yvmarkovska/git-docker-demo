e28b900c0c363c1eec2046f7f777bc07c5621d92
y.markovska <yvmarkovska@gmail.com>
2026-02-12 13:01:26 +0200

    add route

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/team" element={<Team />} />
         <Route path="/organization" element={<Organization />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/keks" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
